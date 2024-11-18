7b8889e20b9fde2e2fcb5a59eaadc37fa2c68de5
Williams Isaac <williamscalg@gmail.com>
2024-11-18 18:49:32 +0000

    bump app version

--- src/background/messages/get-daily-mink-stats.ts
+++ src/background/messages/get-daily-mink-stats.ts
@@ -1,11 +1,15 @@
 import { minkService, userService } from '@/src/background/services';
 import type { PlasmoMessaging } from '@plasmohq/messaging';
+import { scope as sentryScope } from '@/src/lib/sentry';
+
+sentryScope.setTag("service", "messages/get-account-settings.ts");
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   try {
     const stats = await minkService.getDailyMinkStats(req.body.url);
     res.send({ status: true, stats });
   } catch (error) {
+    sentryScope.captureException(error);
     console.error(error);
     res.send({ status: false });
   }
