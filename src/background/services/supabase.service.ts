df85cac699ff63a84a43f9c199aa8e34a8387012
Williams Isaac <williamscalg@gmail.com>
2024-12-14 19:43:52 +0000

    add supabase service file

---
+++ src/background/services/supabase.service.ts
@@ -0,0 +1,40 @@
+import { supabase } from "@/src/core/supabase";
+import { isProduction } from "@/src/misc";
+
+const PRODUCT_ID = isProduction ? "prod_ROXiGSyZunDCn7" : "prod_ROnNOYwZyE174G"
+
+export class SupabaseService {
+    private supabase: SupabaseClient;
+
+    constructor() {
+        this.supabase = supabase;
+    }
+
+    async queryOpenAI(body: any) {
+        const response = await this.supabase.functions.invoke('openai', {
+            body,
+        });
+        return response;
+    }
+
+    async sendEmail(data: any, email: string, subject: string) {
+        const response = await this.supabase.functions.invoke('notifications', {
+            body: {
+                emailData: data,
+                email,
+                subject
+            },
+        }); 
+        return response;
+    }
+
+    async checkSubscription(email: string) {
+        const { data, error } = await this.supabase.functions.invoke('check-subscription', {
+            body: { email, productId: PRODUCT_ID },
+        });
+        if (error) {
+            throw error;
+        }
+        return data;
+    }
+}
